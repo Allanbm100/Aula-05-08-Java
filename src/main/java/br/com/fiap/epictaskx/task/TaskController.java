ee852c1cf20f387a183c1b84871f1225755ece7d
allanbm100 <allan.bm.100@gmail.com>
2025-08-11 21:23:33 -0300

    Aula 11/08 - Início do formulário e index atualizado

--- src/main/java/br/com/fiap/epictaskx/task/TaskController.java
+++ src/main/java/br/com/fiap/epictaskx/task/TaskController.java
@@ -1,11 +1,12 @@
 package br.com.fiap.epictaskx.task;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.config.Task;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -24,5 +25,17 @@ public class TaskController {
     public String index(Model model){
         model.addAttribute("tasks", taskService.getAllTasks());
         return "index";
+    }
+
+    @GetMapping("/form")
+    public String form(){
+        return "form";
+    }
+
+    @PostMapping("/form")
+    public String create(Task task, RedirectAttributes redirect){ //TODO DTO
+        taskService.save(task);
+        redirect.addFlashAttribute("message", "Tarefa cadastrada com sucesso!");
+        return "redirect:/task";
     }
 }
